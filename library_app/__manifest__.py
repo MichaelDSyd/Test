7d2bf99dc299b15a4f85f3c0118715369627ad10
MichaelDSyd <122326413+MichaelDSyd@users.noreply.github.com>
2023-03-30 11:49:45 +1100

    Update __manifest__.py

--- library_app/__manifest__.py
+++ library_app/__manifest__.py
@@ -1,18 +1,18 @@
 {
-  'name': "library_app",
-  'summary': "Test Module",
-  'description': "Test Module, 5% chance to work",
-  'author': "Sydney Tools",
-  'website': "https://sydneytools.com.au/",
-  'category': 'Uncategorised',
-  'version': '0.1',
-  'depends': ['base'],
-  'data': [
-    'security/ir.model.access.csv',
-    'views/views.xml',
-    'views/templates.xml',
+  "name": "library_app",
+  "summary": "Test Module",
+  "description": "Test Module, 5% chance to work",
+  "author": "Michael Daoud",
+  "website": "https://sydneytools.com.au/",
+  "category": 'Uncategorised',
+  "version": "0.1",
+  "depends": ["base"],
+  "data": [
+    "security/ir.model.access.csv",
+    "views/views.xml",
+    "views/templates.xml",
   ],
-  'demo': [
-    'demo/demo.xml',
+  "demo": [
+    "demo/demo.xml",
   ],
 }

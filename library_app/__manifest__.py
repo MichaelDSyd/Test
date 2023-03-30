d618369f5ec5065bab227cc84876b59b760c628c
MichaelDSyd <122326413+MichaelDSyd@users.noreply.github.com>
2023-03-30 11:42:19 +1100

    Update __manifest__.py

--- library_app/__manifest__.py
+++ library_app/__manifest__.py
@@ -1,1 +1,16 @@
-
+{
+  'name': "library_app",
+  'summary': "Test Module",
+  'description': "Test Module, 5% chance to work",
+  'author': "Sydney Tools",
+  'website': "https://sydneytools.com.au/",
+  'category': 'Uncategorised',
+  'version': '0.1',
+  'depends': ['base'],
+  'data': [
+    'security/ir.model.access.csv','views/views.xml','views/templates.xml',\
+  ],
+  'demo': [
+    'demo/demo.xml',
+  ],
+}

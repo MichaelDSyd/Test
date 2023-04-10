dfacd9e6aa5e58805bf2f90b7ee4f4466c2964b0
MichaelDSyd <122326413+MichaelDSyd@users.noreply.github.com>
2023-04-11 09:00:44 +1000

    Update __manifest__.py

--- library_app/__manifest__.py
+++ library_app/__manifest__.py
@@ -1,6 +1,6 @@
 {
-  "name": "ST Library Management",
-  "summary": "ST Manage Library Catalog and Book Lending",
+  "name": "SydT Library Management",
+  "summary": "SydT Manage Library Catalog and Book Lending",
   "author": "Michael Daoud",
   "website": "https://sydneytools.com.au",
   "version": "16.0.1",

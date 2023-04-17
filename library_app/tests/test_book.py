3e70d6f06fcebebb7af9796ed43517ff8d478c91
MichaelDSyd <122326413+MichaelDSyd@users.noreply.github.com>
2023-04-17 12:20:38 +1000

    Added bracket

--- library_app/tests/test_book.py
+++ library_app/tests/test_book.py
@@ -19,4 +19,4 @@ class TestBook(TransactionCase):
             self.Book = self.env["library.book"]
             self.book_ode = self.Book.create({
                 "name": "Odoo Development Essentials",
-                "isbn": "879-1-78439-279-6"})
+                "isbn": "879-1-78439-279-6"}))

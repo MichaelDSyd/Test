84bdf90a3e784fa7a8389adcbb46771398bdc016
MichaelDSyd <122326413+MichaelDSyd@users.noreply.github.com>
2023-04-19 09:19:18 +1000

    Removed Error Causing Things

--- library_app/tests/test_book.py
+++ library_app/tests/test_book.py
@@ -9,14 +9,3 @@ class TestBook(TransactionCase):
     def test_book_create(self):
         "New Books are active by default"
         self.assertEqual(self.book1.active, True)
-    def test_check_isbn(self):
-        "Check valid ISBN"
-        self.assertTrue(self.book1._check_isbn)
-    def setUp(self, *args, **kwargs):
-        super().setUp(*args, **kwargs)
-        user_admin = self.env.ref("base.user_admin")
-        self.env = self.env(user=user_admin)
-        self.Book = self.env["library.book"]
-        self.book_ode = self.Book.create({
-            "name": "Odoo Development Essentials",
-            "isbn": "879-1-78439-279-6"})

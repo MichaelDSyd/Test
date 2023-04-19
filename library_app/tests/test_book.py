d6189fe2746f713505e183659ac7b9bd6c6bf38b
MichaelDSyd <122326413+MichaelDSyd@users.noreply.github.com>
2023-04-19 11:51:58 +1000

    reverted changes

--- library_app/tests/test_book.py
+++ library_app/tests/test_book.py
@@ -3,15 +3,15 @@ class TestBook(TransactionCase):
     def setUp(self, *args, **kwargs):
         super().setUp(*args, **kwargs)
         self.Book = self.env["library.book"]
-        self.book = self.Book.create({
+        self.book1 = self.Book.create({
             "name": "Odoo Development Essentials",
             "isbn": "879-1-78439-279-6"})
     def test_book_create(self):
         "New Books are active by default"
-        self.assertEqual(self.book.active, True)
+        self.assertEqual(self.book1.active, True)
     def test_check_isbn(self):
         "Check valid ISBN"
-        self.assertTrue(self.book._check_isbn)
+        self.assertTrue(self.book1._check_isbn)
     def setUp(self, *args, **kwargs):
         super().setUp(*args, **kwargs)
         user_admin = self.env.ref("base.user_admin")

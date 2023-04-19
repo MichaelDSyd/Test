8eb12dfe08a31786fa16a17c5cca35162a2f663b
MichaelDSyd <122326413+MichaelDSyd@users.noreply.github.com>
2023-04-19 11:00:14 +1000

    Fixed ISBN Check

--- library_app/models/library_book.py
+++ library_app/models/library_book.py
@@ -14,7 +14,7 @@ class Book(models.Model):
 
 #Business Logic to Check ISBN
 
-  def _check_isbn(self) :
+  def _check_isbn(self):
       self.ensure_one()
       digits = [int(x) for x in self.isbn if x.isdigit()]
       if len(digits) == 13

ff9486c5308004c67f200176ebe2d83037daa3a1
MichaelDSyd <122326413+MichaelDSyd@users.noreply.github.com>
2023-04-19 10:58:12 +1000

    Added Button Business Logic

--- library_app/models/library_book.py
+++ library_app/models/library_book.py
@@ -11,3 +11,16 @@ class Book(models.Model):
       string="Publisher")
     author_ids = fields.Many2many("res.partner",
       string="Authors")
+
+#Business Logic to Check ISBN
+
+  def _check_isbn(self) :
+      self.ensure_one()
+      digits = [int(x) for x in self.isbn if x.isdigit()]
+      if len(digits) == 13
+          ponderations = [1, 3] * 6
+          terms = [a * b for a, b in zip(digits[:12],
+            ponderations)]
+          remain = sum(terms) % 10
+          check = 10 - remain if remain != 0 else 0
+          return digits [-1] == check

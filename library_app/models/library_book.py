89374b18953cabd8a36d4bd4c1c042b53f4300bf
MichaelDSyd <122326413+MichaelDSyd@users.noreply.github.com>
2023-04-19 11:20:34 +1000

    Added Validation Error

--- library_app/models/library_book.py
+++ library_app/models/library_book.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from odoo.exceptions import ValidationError
 class Book(models.Model):
     _name = "library.book"
     _description = "Book"
@@ -14,13 +15,23 @@ class Book(models.Model):
 
 #Business Logic to Check ISBN
 
-def _check_isbn(self):
-    self.ensure_one()
-    digits = [int(x) for x in self.isbn if x.isdigit()]
-    if len(digits) == 13:
-        ponderations = [1, 3] * 6
-        terms = [a * b for a, b in zip(digits[:12],
-          ponderations)]
-        remain = sum(terms) % 10
-        check = 10 - remain if remain != 0 else 0
-        return digits [-1] == check
+    def _check_isbn(self):
+        self.ensure_one()
+        digits = [int(x) for x in self.isbn if x.isdigit()]
+        if len(digits) == 13:
+            ponderations = [1, 3] * 6
+            terms = [a * b for a, b in zip(digits[:12],
+              ponderations)]
+            remain = sum(terms) % 10
+            check = 10 - remain if remain != 0 else 0
+            return digits [-1] == check
+
+#Validation Error Code
+
+    def button_check_isbn(self):
+        for book in self:
+            if not book.isbn:
+                raise ValidationError("please provide an ISBN for %s" % book.name)
+            if book.isbn and not book._checkisbn():
+                raise ValidationError("%s ISBN is invalid" % book.isbn)
+        return True 

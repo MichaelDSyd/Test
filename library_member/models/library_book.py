1093ee486ddea56e52b56e0bc33d12401220b2c8
MichaelDSyd <122326413+MichaelDSyd@users.noreply.github.com>
2023-04-21 09:57:03 +1000

    Updated

--- library_member/models/library_book.py
+++ library_member/models/library_book.py
@@ -3,5 +3,3 @@ from odoo import fields, models
 class Book(models.Model):
     _inherit = "library.book"
     is_available = fields.Boolean("Is Available?")
-    #TEST BELOW
-    test_field = fields.Char("Test Text:")

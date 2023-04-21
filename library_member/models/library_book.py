836f6a1792cf19e03005a2dd14503dcaae4b4c3c
MichaelDSyd <122326413+MichaelDSyd@users.noreply.github.com>
2023-04-21 11:54:34 +1000

    Updated library_book with # comments

--- library_member/models/library_book.py
+++ library_member/models/library_book.py
@@ -3,12 +3,12 @@ from odoo import fields, models
 class Book(models.Model):
     _inherit = "library.book"
     is_available = fields.Boolean("Is Available?")
-    #TEST CHAR FIELD BELOW
+#TEST CHAR FIELD BELOW
     test_char = fields.Char("Test Char Field")
-    #ISBN 13 or 10 VALID
+#ISBN 13 or 10 VALID
     isbn = fields.Char(help="use a valid ISB-13 or ISB-10.")
     publisher_id = fields.Many2one(index=True)
-
+#_CHECK_ISBN 10 DIGIT
     def _check_isbn(self):
         self.ensure_one()
         digits = [int(x) for x in self.isbn if x.isdigit()]

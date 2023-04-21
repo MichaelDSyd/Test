bb812ea8dc58cd7d8bf441becd6622f928c3c074
MichaelDSyd <122326413+MichaelDSyd@users.noreply.github.com>
2023-04-21 11:27:45 +1000

    Added ISB 13 and 10 to library_book.py

--- library_member/models/library_book.py
+++ library_member/models/library_book.py
@@ -5,3 +5,6 @@ class Book(models.Model):
     is_available = fields.Boolean("Is Available?")
     #TEST CHAR FIELD BELOW
     test_char = fields.Char("Test Char Field")
+    #ISBN 13 or 10 VALID
+    isbn = fields.Char(help="use a valid ISB-13 or ISB-10.")
+    publisher_id = fields.Many2one(index=True)

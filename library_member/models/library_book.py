2e24f0476fdf89406a7ace3a0565712b6bd3aed1
MichaelDSyd <122326413+MichaelDSyd@users.noreply.github.com>
2023-04-21 11:11:05 +1000

    Added test_char field

--- library_member/models/library_book.py
+++ library_member/models/library_book.py
@@ -3,3 +3,5 @@ from odoo import fields, models
 class Book(models.Model):
     _inherit = "library.book"
     is_available = fields.Boolean("Is Available?")
+    #TEST CHAR FIELD BELOW
+    test_char = fields.Char("Test Char Field")

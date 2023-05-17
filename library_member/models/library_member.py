1c548f5570daf485d411ba2e627175ab81d7f0a7
MichaelDSyd <122326413+MichaelDSyd@users.noreply.github.com>
2023-05-17 10:30:27 +1000

    added library_member.py info

--- library_member/models/library_member.py
+++ library_member/models/library_member.py
@@ -2,3 +2,12 @@ from odoo import fields, models
 
 class Member(models.Model):
     _name = "library.member"
+    _description = "Library Member"
+    _inherets = {"res.partner": "partner_id"}
+
+    card_number = fields.Char()
+    partner_id = fields.Many2one(
+    "res.partner",
+    delegate=True,
+    ondelete="cascade",
+    required=True)

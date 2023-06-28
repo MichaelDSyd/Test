2f23bb23b460259d9db0cb18fea715e908121ccd
MichaelDSyd <122326413+MichaelDSyd@users.noreply.github.com>
2023-06-28 10:15:39 +1000

    updated __manifest__.py issue

--- library_checkout/__manifest__.py
+++ library_checkout/__manifest__.py
@@ -1,6 +1,6 @@
 { "name": "Library Book Checkout",
   "description": "Members can borrow books from the library.",
-  "author": "Daniel Reis"
+  "author": "Daniel Reis",
   "depends": ["library_member"],
   "data": [
     "security/ir.model.access.csv",

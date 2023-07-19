23f9c0ec7c87673c91cdabf7726fbae91cd91680
MichaelDSyd <122326413+MichaelDSyd@users.noreply.github.com>
2023-07-19 10:46:29 +1000

    Added Api.Model in library_checkout.py

--- library_checkout/models/library_checkout.py
+++ library_checkout/models/library_checkout.py
@@ -21,3 +21,19 @@ class Checkout(models.Model):
         "checkout_id",
         string="Borrowed Books",
     )
+    #Added api.model and stage_id/state
+    @api.model
+    def _default_stage_id(self):
+        Stage = self.env ["library.checkout.stage"]
+        return Stage.search([("state","=","new")],
+          limit=1)
+    @api.model
+    def _group_expand_stage_id(self, stages, domain,
+      order):
+        return stages.search([], order=order)
+    stage_id = fields.Many2one(
+        "library.checkout.stage",
+        default=_default_stage_id,
+        group_expand="_group_expand_stage_id")
+    state = fields.Selection(related="stage_id.state")
+        
